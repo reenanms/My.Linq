c9f6c70ae9e2db3117fd645f6515c66a3c4bb0e5
Luciano Zimmermann <luckzim@gmail.com>
2020-05-26 23:46:08 -0300

    Including Take Extension Method

--- My.Linq.Test/LinqExtensions.cs
+++ My.Linq.Test/LinqExtensions.cs
@@ -55,5 +55,37 @@ namespace My.Linq.Test
 
             Assert.True(myResult);
         }
+
+        [Fact(DisplayName = "Take two items from array of numbers")]
+        public void TakeTwoFromArrayOfNumbers()
+        {
+            var myListOfNumbers = new[] { 1, 2, 3, 4, 5, 6, 7 };
+
+            var myResult = myListOfNumbers.Take(2);
+
+            var myListOfExpectNumbers = new[] { 1, 2 };
+            Assert.Equal(myListOfExpectNumbers, myResult);
+        }
+
+        [Fact(DisplayName = "Take zero items from array of numbers")]
+        public void TakeZeroFromArrayOfNumbers()
+        {
+            var myListOfNumbers = new[] { 1, 2, 3, 4, 5, 6, 7 };
+
+            var myResult = myListOfNumbers.Take(0);
+
+            Assert.Empty(myResult);
+        }
+
+        [Fact(DisplayName = "Take three items from array of strings")]
+        public void TakeThreeFromArrayOfStrings()
+        {
+            var myListOfStrings = new[] { "A", "B", "C", "D", "E" };
+
+            var myResult = myListOfStrings.Take(3);
+
+            var myListOfExpectStrings = new[] { "A", "B", "C" };
+            Assert.Equal(myListOfExpectStrings, myResult);
+        }
     }
 }
